b53027ceaffb9bdb50ffd37b7a1c08bb3ac1f7b8
Steven Matchett <steve.matchett@move.com>
2021-04-08 20:39:11 -0400

    init

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const express = require('express')
+const find = require('local-devices');
 
 function createWindow () {
   // Create the browser window.
+  findTV();
+  createExpress();
+  // createExpress();
+    
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 430,
+    height: 1100,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -37,5 +43,140 @@ app.on('activate', function () {
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
+
+
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
+const Samsung = require('samsung-tv-control').default
+const { KEYS } = require('samsung-tv-control/lib/keys')
+const { APPS } = require('samsung-tv-control/lib/apps')
+let loaded = false;
+let config = {}
+
+let runKey = (key) => {
+  const control = new Samsung(config)
+
+  control.turnOn()
+  control
+    .isAvaliable()
+    .then(() => {
+      // Send key to TV
+      control.sendKey(key, function(err, res) {
+          if (err) {
+            throw new Error(err)
+          } else {
+            console.log(res)
+          }
+      
+      });
+    })
+    .catch(e => console.error(e))
+  
+}
+
+function getConfig(ip, mac){
+  return {
+    debug: true, // Default: false
+    ip: ip,
+    mac: mac.replace(/:/g, ""),
+    name: 'NodeJS-Test', // Default: NodeJS
+    port: 8001, // Default: 8002
+    token: '12345678',
+  }
+}
+
+async function findTV(){
+
+  let devices = await find();
+  for (let device of devices){
+    try {
+      let configCandidate = getConfig(device.ip, device.mac);
+      const control = new Samsung(configCandidate)
+      control.turnOn()
+      if (await control.isAvaliable()){
+        config = configCandidate;
+      }
+    } catch (e) {
+      console.log("failed to connect", device.ip)
+    }
+  }
+  console.log(config)
+}
+
+
+function createExpress(){
+  const app = express()
+  const port = 3000
+
+  app.get('/ready', (req, res) => {
+    console.log(config)
+    if (config.ip){
+      res.json({...config,})
+    } else {
+      res.send()
+    }
+  })
+
+  app.get('/volume/up', (req, res) => {
+    runKey(KEYS.KEY_VOLUP);
+    res.send({sucess:true})
+  })
+  
+  app.get('/volume/down', (req, res) => {
+    runKey(KEYS.KEY_VOLDOWN);
+    res.send({sucess:true})
+  })
+
+  app.get('/nav/left', (req, res) => {
+    runKey(KEYS.KEY_LEFT);
+    res.send({sucess:true})
+  })
+
+  app.get('/nav/right', (req, res) => {
+    runKey(KEYS.KEY_RIGHT);
+    res.send({sucess:true})
+  })
+
+  app.get('/nav/down', (req, res) => {
+    runKey(KEYS.KEY_DOWN);
+    res.send({sucess:true})
+  })
+
+  app.get('/nav/up', (req, res) => {
+    runKey(KEYS.KEY_UP);
+    res.send({sucess:true})
+  })
+
+  app.get('/nav/menu', (req, res) => {
+    runKey(KEYS.KEY_MENU);
+    res.send({sucess:true})
+  })
+
+  app.get('/nav/back', (req, res) => {
+    runKey(KEYS.KEY_BACK_MHP);
+    res.send({sucess:true})
+  })
+
+  app.get('/nav/enter', (req, res) => {
+    runKey(KEYS.KEY_ENTER);
+    res.send({sucess:true})
+  })
+
+  app.get('/nav/back', (req, res) => {
+    runKey(KEYS.KEY_BACK_MHP);
+    res.send({sucess:true})
+  })
+
+  app.get('/power', (req, res) => {
+    runKey(KEYS.KEY_POWER);
+    res.send({sucess:true})
+  })
+
+
+  
+
+  app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+}
--- preload.js
+++ preload.js
@@ -1,12 +1,61 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+  addOnlick("vol-up", "/volume/up")
+  addOnlick("vol-down", "/volume/down") 
+  addOnlick("power", "/power") 
+  addOnlick("back", '/nav/back')
+  addOnlick("up", "/nav/up") 
+  addOnlick("down", "/nav/down") 
+  addOnlick("left", "/nav/left") 
+  addOnlick("right", "/nav/right") 
+  addOnlick("enter", "/nav/enter") 
 })
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        // up arrow
+        callApi("/nav/up")
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+        callApi("/nav/down")
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+       callApi("/nav/left")
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+       callApi("/nav/right")
+    } else if (e.keyCode == '13') {
+      // enter
+      callApi("/nav/enter")
+      e.stopPropagation();
+    } else if (e.keyCode == '46'){
+      callApi('/nav/back')
+    }
+
+}
+
+function callApi(url){
+  var oReq = new XMLHttpRequest();
+  oReq.open("GET", "http://localhost:3000" + url);
+  oReq.send();
+}
+
+function addOnlick(id, url) {
+  var button = document.getElementById(id);
+  
+  button.onclick = function(){
+      callApi(url)
+    	// fetch("http://localhost:3000/volume/up")
+   }
+}
+
